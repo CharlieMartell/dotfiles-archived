5f2cfc85ae1332cb4b337dedd42fcc66c73714c2
CharlieMartell <charliecmartell@gmail.com>
2015-08-09 19:58:23 -0700

    dotfile big update

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Installing dotfiles"
+
+echo "Initializing submodule(s)"
+git submodule update --init --recursive
+
+source install/link.sh
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Running on OSX"
+
+    echo "Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    echo "Brewing all the things"
+    source install/brew.sh
+
+    echo "Updating OSX settings"
+    source install/osx.sh
+fi
+
+echo "Configuring zsh as default shell"
+chsh -s $(which zsh)
+
+echo "Done."
---
+++ install/brew.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# cli tools
+brew install ack
+brew install tree
+brew install wget
+
+# development tools
+brew install git
+brew install hub
+brew install macvim --override-system-vim
+brew install reattach-to-user-namespace
+brew install tmux
+brew install zsh
+brew install highlight
+brew install nvm
+brew install z
+brew install markdown
